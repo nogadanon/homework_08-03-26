7311b9efcd8082c8d95b2d41542e57afcbd56c7f
Noga <nogakol1@gmail.com>
2026-03-10 14:43:28 +0200

    homework

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+# 1
+
+def get_lucky_numbers(amount: int) -> tuple[int]:
+    l1 = []
+    for i in range(amount):
+        l1.append(random.randint(1, 100))
+    return tuple(l1)
+
+print(get_lucky_numbers(5))
+
+# 2
+
+def input_until_lucky(lucky_numbers: tuple) -> int:
+    i = 0
+    while True:
+        guess = input('guess a lucky number: ')
+        if not guess.isdigit():
+            print('invalid input')
+            continue
+        else:
+            i += 1
+        if int(guess) in lucky_numbers:
+            return i
+
+
+tup1 = (3, 6, 10, 20, 56)
+
+print(input_until_lucky(tup1))
+
+
